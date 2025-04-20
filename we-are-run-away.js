c8b2996ae76ce3d2f5660f4ba1c8011d156a6242
shenlingfeng233 <WCshenmo@outlook.com>
2025-04-20 20:09:03 +0800

    Update we-are-run-away.js

--- we-are-run-away.js
+++ we-are-run-away.js
@@ -1,4 +1,14 @@
 var a = document.getElementById("QQ");
+var b = document.getElementsByTagName("h1");
+var colors = ["yellow", "green", "purple", "white"]
+var time = 1
 a.onclick = function(){
-    window.location.href = "https://qm.qq.com/q/YtpneR0UMI"
+    window.location.href = "https://qm.qq.com/q/YtpneR0UMI";
 }
+setInterval(function(){
+    console.error(time);
+    time++
+    if (time == 100){
+        window.location.href = "https://qm.qq.com/q/YtpneR0UMI";
+    }
+},1000)
